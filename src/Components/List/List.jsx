d4dd80c6a8a0b2c57e14dc3458c17b525185d06a
Anukriti <anukriti.student.che21@itbhu.ac.in>
2024-10-01 23:16:28 +0530

    update

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -83,9 +83,6 @@ export default function List(props) {
                     <div className="list-add-item">
                     <img src={add} alt='tag'/>
                     </div>
-                    {/* <div className="list-option-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path fill="currentColor" d="M10.001 7.8a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 10 7.8zm-7 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 3 7.8zm14 0a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 17 7.8z"/></svg>
-                    </div> */}
                 </div>
             </div>
 
