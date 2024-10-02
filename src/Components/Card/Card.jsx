385b20d4316f79a74640b179035aa3d73ea81327
Anukriti <anukriti.student.che21@itbhu.ac.in>
2024-10-02 10:44:36 +0530

    update card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './Card.css';
 import high_Priority from '../../Assets/Images/high_Priority.svg';
 import low_Priority from '../../Assets/Images/low_Priority.svg';
@@ -7,64 +6,67 @@ import medium_Priority from '../../Assets/Images/medium_Priority.svg';
 import no_Priority from '../../Assets/Images/no_priority.svg';
 import urgent_Priority_grey from '../../Assets/Images/urgent_Priority_grey.svg';
 
-const AnoopProfileImg = "https://i.pinimg.com/originals/07/33/ba/0733ba760b29378474dea0fdbcb97107.png";
-const YogeshProfileimg = "https://thumbs.dreamstime.com/b/profile-picture-caucasian-male-employee-posing-office-happy-young-worker-look-camera-workplace-headshot-portrait-smiling-190186649.jpg";
+const profileImages = {
+  "Anoop sharma": "https://i.pinimg.com/originals/07/33/ba/0733ba760b29378474dea0fdbcb97107.png",
+  "Yogesh": "https://thumbs.dreamstime.com/b/profile-picture-caucasian-male-employee-posing-office-happy-young-worker-look-camera-workplace-headshot-portrait-smiling-190186649.jpg"
+};
+
+const priorityIcons = {
+  0: no_Priority,
+  1: low_Priority,
+  2: medium_Priority,
+  3: high_Priority,
+  4: urgent_Priority_grey
+};
 
 export default function Card({ cardDetails }) {
+  const { id, userObj, title, priority, tag } = cardDetails;
+  const userImage = profileImages[userObj.name];
+
+  // Helper function to get user initials
+  const getUserInitials = (name) => {
+    return name
+      .split(' ')
+      .map((n) => n[0])
+      .join('');
+  };
+
   return (
-    <>
-      <div className="card-container">
-        <div className="card-id-wrapper">
-          <div className="card-id">{cardDetails.id}</div>
-          <div className="card-profile">
-            <div className="card-profile-initial">
-              {/* Check the user's name to display the correct image or fallback */}
-              {cardDetails.userObj.name === "Anoop sharma" ? (
-                <img className="card-profile-image" src={AnoopProfileImg} alt="Anoop" />
-              ) : cardDetails.userObj.name === "Yogesh" ? (
-                <img className="card-profile-image" src={YogeshProfileimg} alt="Yogesh" />
-              ) : (
-                // Render the initials if it's neither "Anoop" nor "Yogesh"
-                <div className="card-profile-initial-fallback">
-                  {cardDetails.userObj.name
-                    .split(' ')
-                    .map((n) => n[1])
-                    .join('')}
-                </div>
-              )}
-            </div>
-            <div
-              className={
-                cardDetails.userObj.available
-                  ? 'card-profile-initial-available card-profile-initial-available-true'
-                  : 'card-profile-initial-available'
-              }
-            ></div>
-          </div>
-        </div>
-
-        <div className="card-title">{cardDetails.title}</div>
-
-        <div className="card-tag">
-          {
-            {
-              0: <div className="card-tag-icon"><img src={no_Priority} alt="No Priority" /></div>,
-              1: <div className="card-tag-icon"><img src={low_Priority} alt="Low Priority" /></div>,
-              2: <div className="card-tag-icon"><img src={medium_Priority} alt="Medium Priority" /></div>,
-              3: <div className="card-tag-icon"><img src={high_Priority} alt="High Priority" /></div>,
-              4: <div className="card-tag-icon"><img src={urgent_Priority_grey} alt="Urgent Priority" /></div>
-            }[cardDetails.priority]
-          }
-
-          {cardDetails.tag.map((tag, index) => {
-            return (
-              <div key={index} className="card-tag-box">
-                <div className="card-tag-title">{tag}</div>
+    <div className="card-container">
+      <div className="card-id-wrapper">
+        <div className="card-id">{id}</div>
+        <div className="card-profile">
+          <div className="card-profile-initial">
+            {userImage ? (
+              <img className="card-profile-image" src={userImage} alt={userObj.name} />
+            ) : (
+              <div className="card-profile-initial-fallback">
+                {getUserInitials(userObj.name)}
               </div>
-            );
-          })}
+            )}
+          </div>
+          <div
+            className={
+              userObj.available
+                ? 'card-profile-initial-available card-profile-initial-available-true'
+                : 'card-profile-initial-available'
+            }
+          ></div>
         </div>
       </div>
-    </>
+
+      <div className="card-title">{title}</div>
+
+      <div className="card-tag">
+        <div className="card-tag-icon">
+          <img src={priorityIcons[priority]} alt="Priority" />
+        </div>
+        {tag.map((tagItem, index) => (
+          <div key={index} className="card-tag-box">
+            <div className="card-tag-title">{tagItem}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
